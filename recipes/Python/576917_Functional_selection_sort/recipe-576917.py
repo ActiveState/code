702450dbdfd193d825ae1acca5c7320c92d32543
kgashok <kgashok@users.noreply.github.com>
2017-08-15 15:52:49 +0530

    Update recipe-576917.py
    Added the recursive version from the comments at https://code.activestate.com/recipes/576917-functional-selection-sort/#c1

--- recipes/Python/576917_Functional_selection_sort/recipe-576917.py
+++ recipes/Python/576917_Functional_selection_sort/recipe-576917.py
@@ -15,3 +15,9 @@ selection_sort = lambda sort_list: reduce(
 test_list = [ 5, 1, 7, 0, -3, -10, 10, -6, 1, 0, 2, 4, -2, 6, 5, 8, 2]
 print test_list
 print selection_sort(test_list)
+
+# Recursive version
+def select_sort_r(L):
+    if not L: return [] # terminal case 
+    idx, v = min(enumerate(L), key=lambda e: e[1]) # select the minimum
+    return [v] + select_sort_r(L[:idx] + L[idx+1:]) # recursively call on the remainder
