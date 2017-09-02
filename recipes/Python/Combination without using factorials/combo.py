4affc91538ce737f055a3b49c1cf539610a68e5b
Muhammad Arslan <rslnkrmt2552@gmail.com>
2017-09-02 22:17:05 +0500

    Combinations without using factorials-Python
    Signed-off-by: Muhammad Arslan <rslnkrmt2552@gmail.com>

---
+++ recipes/Python/Combination without using factorials/combo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import operator as op
+
+
+def ncr(n, r):
+    r       = min(r, n-r)
+    if x == 0: return 1
+    num     = reduce(op.mul, xrange(n, n-1, -1))
+    denom   = reduce(op.mul, xrange(1, r+1))
+    return num // denom
