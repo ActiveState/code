e463b980a31c094a702496d1057ad593b6ccad91
kekePower <kekePower@users.noreply.github.com>
2017-09-02 17:51:18 +0200

    Fixed a new-line issue with lynx
    Lynx displayed a new-line after the expected output. This addition fixes this issue.

--- recipes/Bash/578578_Get_external_IP/recipe-578578.sh
+++ recipes/Bash/578578_Get_external_IP/recipe-578578.sh
@@ -1,5 +1,5 @@
 wget -qO - http://icanhazip.com | grep ^
 # or
-lynx --dump http://icanhazip.com | grep ^
+lynx --dump http://icanhazip.com | grep ^ | head -1
 # or
 curl -s http://icanhazip.com | grep ^
