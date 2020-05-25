6c30c95311f2c47dce1d3f1d62965f275a9f092c
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-05-25 08:10:04 +0000

    refactored to make it more generic for any range

--- recipes/Python/576612_FizzBuzz/recipe-576612.py
+++ recipes/Python/576612_FizzBuzz/recipe-576612.py
@@ -1,5 +1,10 @@
-n=map(str, range(101))
-n[::3]=['Fizz']*34
-n[::5]=['Buzz']*21
-n[::15]=['FizzBuzz']*7
-print '\n'.join(n[1:])
+from __future__ import division 
+from math import ceil
+
+N = 101
+n = map(str, range(N))
+n[::3]=['Fizz']*int(ceil(N/3))
+n[::5]=['Buzz']*int(ceil(N/5))
+n[::15]=['FizzBuzz']*int(ceil(N/15))
+
+print '\n'.join(str(n) for n in n[1:])
