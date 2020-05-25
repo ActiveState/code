899c249f2a1510aafe26cacb15ffe7f82784eba3
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-05-25 08:13:38 +0000

    refactored to eliminate third slicing operation

--- recipes/Python/576612_FizzBuzz/recipe-576612.py
+++ recipes/Python/576612_FizzBuzz/recipe-576612.py
@@ -2,9 +2,8 @@ from __future__ import division
 from math import ceil
 
 N = 101
-n = map(str, range(N))
+n = range(N)
 n[::3]=['Fizz']*int(ceil(N/3))
-n[::5]=['Buzz']*int(ceil(N/5))
-n[::15]=['FizzBuzz']*int(ceil(N/15))
+n[::5]= [ t + "Buzz" if type(t) == str else "Buzz" for t in n[::5] ] 
 
 print '\n'.join(str(n) for n in n[1:])
